5d6e197e83daf522ac895a8422f8a30f014eb091
Dr.Dados <adnane.bachchar@gmail.com>
2023-10-27 11:59:49 +0100

    adding slider

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 });
 
 allSections.forEach(function (section) {
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
   sectionObserver.observe(section);
 });
 
@@ -150,6 +150,65 @@ const imgObserver = new IntersectionObserver(revealImg, {
 });
 lazyImgs.forEach(el => {
   imgObserver.observe(el);
+});
+
+// Slider
+
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+let curSlide = 0;
+const maxSlide = slides.length;
+const slider = document.querySelector('.slider');
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+goToSlide(0);
+//Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) curSlide = 0;
+  else curSlide++;
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) curSlide = maxSlide - 1;
+  else curSlide--;
+  goToSlide(curSlide);
+};
+const dotContainer = document.querySelector('.dots');
+const createDots = function () {
+  slides.forEach((_, i) => {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+createDots();
+const activateDots = function (slide) {
+  const dots = document.querySelectorAll('.dots__dot');
+  dots.forEach(dot => {
+    dot.classList.remove('dots__dot--active');
+  });
+  slide.classList.add('dots__dot--active');
+};
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'ArrowRight') nextSlide();
+  if (e.key === 'ArrowLeft') prevSlide();
+});
+
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activateDots(e.target);
+  }
 });
 //////////////////// Lecture //////////////////////
 // const allButtons = document.getElementsByTagName('button');
